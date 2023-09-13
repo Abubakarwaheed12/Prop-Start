a6a67985d7c9416bf5e0e1c8afee37c7fd991cf9
abubakarwaheed12 <abubakarjutt6346527@gmail.com>
2023-09-14 01:48:56 +0500

    sd

--- accounts/emails.py
+++ accounts/emails.py
@@ -4,7 +4,7 @@ from django.conf import settings
 def send_otp_email(email, subject, message):
     
     from_email = settings.EMAIL_HOST_USER
-    print(from_email)
+    print('Send Email to the user')
     recipient_list = [email]
     send_mail(subject, message, from_email, recipient_list, fail_silently=False)
 
