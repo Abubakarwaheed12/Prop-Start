f0ced05e0e004fb7a8048f080a8e8e55d9effa0a
Mohsin005 <hafizjee105@gmail.com>
2023-09-25 07:51:52 -0400

    update

--- core/settings.py
+++ core/settings.py
@@ -83,11 +83,16 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': env.db()
+# }
+
 DATABASES = {
-    'default': env.db()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
