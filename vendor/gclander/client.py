e153370255ecddf0fdcb34d58a3bb31e8c99ecf2
abubakarwaheed12 <abubakarjutt6346527@gmail.com>
2023-10-07 17:22:14 +0500

    Add Calender

--- vendor/gclander/client.py
+++ vendor/gclander/client.py
@@ -6,67 +6,41 @@ import datetime
 from datetime import timedelta
 import pytz
 
-SCOPES = ["https://www.googleapis.com/auth/calendar"]
-credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    filename="./gclient_secret.json", scopes=SCOPES
-)
 
-def build_service():
-    service = build("calendar", "v3", credentials=credentials)
-    return service
 
 
 class GoogleAPIClient:
 
     def __init__(self):
-        SCOPES = ['https://www.googleapis.com/auth/calendar']
 
-        # Load the service account credentials with delegation
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
         credentials = service_account.Credentials.from_service_account_file(
             './gclient_secret.json',
             scopes=SCOPES,
         )
-        credentials = credentials.with_subject('propstart@propstart.iam.gserviceaccount.com')
-        service_account_email = "abubakarjutt6346527@gmail.com"
+        credentials = credentials.with_subject('noreply@propstart.com.au')
+        service_account_email = "noreply@propstart.com.au"
         
-        # Create the Calendar API client
 
         self.service = build('calendar', 'v3', credentials=credentials)
     # start_datetime.isoformat()
-
     def gc_event(self, title, description, attendee):
-        service = build_service()
-        # event = {
-        #     'summary': title,
-        #     'description': description,
-        #     'start': {'dateTime': '2023-10-06T14:00:00', 'timeZone': 'UTC'},
-        #     'end': {'dateTime': '2023-10-06T14:30:00', 'timeZone': 'UTC'},
-        #     'attendees': [{'email': 'noreply@propstart.com.au'}] + [{'email': attendee}],
-        #     'reminders': {
-        #         'useDefault': False,
-        #         'overrides': [
-        #             {'method': 'email', 'minutes': 30},  # Send an email notification 30 minutes before the event
-        #         ],
-        #     },
-        # }
-        # # Insert the event into the calendar
-        # created_event = self.service.events().insert(calendarId='primary', body=event).execute()
-        # print('created_event', created_event)
-
-        # return created_event
+        
+        service = self.service
         start_datetime = datetime.datetime.now(tz=pytz.utc)
         event = (
             service.events()
             .insert(
-                calendarId="0a45566e61f1be80bc686325639071dabc79547ac76358b49576b59c0ccc4be8@group.calendar.google.com",
+                calendarId="noreply@propstart.com.au",
                 body={
-                    "summary": "Book Call",
-                    "description": "Book A Call Event",
+                    "summary": "Book A Call",
+                    "description": "Book A Call Meeting",
                     "start": {"dateTime": start_datetime.isoformat()},
                     "end": {
                         "dateTime": (start_datetime + timedelta(minutes=15)).isoformat()
                     },
-                    "attendees": [{"email":"hamidnwl123@gmail.com",  "responseStatus": "needsAction"}],
+                    "attendees": [{"email":"abubakarjutt6346527@gmail.com", }],
+                    'sendNotifications': True,
                 },
             )
             .execute()
